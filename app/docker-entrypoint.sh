b4e1898c01cad4948a3ace4696c52a70b0551f62
Mikhail Sobolev <kseon@inbox.ru>
2024-06-28 01:37:18 +0400

    comp_docker-compose

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+#uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn app.main:app --host 192.168.55.5 --port 8000 --reload
